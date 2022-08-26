fc68d2272bfce766c10e657a1d09f51292b2527f
unknown <isabella14brandao@gmail.com>
2022-08-26 09:58:52 -0300

    inseriondo inner join a excercicio games

--- exercicios-entrega_My_SQL/exercicio1-game.sql
+++ exercicios-entrega_My_SQL/exercicio1-game.sql
@@ -9,16 +9,20 @@ USE db_generation_games_online;
 CREATE TABLE tb_classes(
 	id bigint auto_increment primary key,
     posicao_ataque varchar(50),/*se vai ser arqueiro, atirador...*/
-    posicao_jogo varchar(50)/*se vai ser front ou back*/
+    posicao_jogo varchar(50),
+    posicao boolean/*se vai ser front ou back*/
 	
 );
+/*aDICIONANDO CAMPO BOOLEAN*/
+ALTER TABLE tb_classes ADD posicao boolean;
+
 
 /*Insira  registros na tabela tb_personagens*/
 
 CREATE TABLE tb_personagens(
 	id bigint auto_increment primary key,
-    poder_ataque decimal(7,2),
-    poder_defesa decimal(7,2),
+    poder_ataque decimal(9,2),
+    poder_defesa decimal(9,2),
     nome varchar(50),
     arma varchar (50),
     classe_id bigint,
@@ -29,11 +33,11 @@ CREATE TABLE tb_personagens(
 
 SELECT * FROM tb_classes;
 
-INSERT INTO tb_classes(posicao_ataque, posicao_jogo)VALUES("Arqueiro","Front");
-INSERT INTO tb_classes(posicao_ataque,posicao_jogo)VALUES("Acrobata","Back");
-INSERT INTO tb_classes(posicao_ataque,posicao_jogo)VALUES("Ratreador","Front");
-INSERT INTO tb_classes(posicao_ataque,posicao_jogo)VALUES("Espadachim","Front");
-INSERT INTO tb_classes(posicao_ataque,posicao_jogo)VALUES("Arqueiro","Back");
+INSERT INTO tb_classes(posicao_ataque,posicao_jogo,posicao)VALUES("Arqueiro","Front",true);
+INSERT INTO tb_classes(posicao_ataque,posicao_jogo,posicao)VALUES("Acrobata","Back",false);
+INSERT INTO tb_classes(posicao_ataque,posicao_jogo,posicao)VALUES("Ratreador","Front",true);
+INSERT INTO tb_classes(posicao_ataque,posicao_jogo,posicao)VALUES("Espadachim","Front",true);
+INSERT INTO tb_classes(posicao_ataque,posicao_jogo,posicao)VALUES("Arqueiro","Back",false);
 
 
 /*Insira 8 registros na tabela tb_personagens, preenchendo a Chave Estrangeira para criar a relação com a tabela tb_classes.*/
@@ -43,9 +47,9 @@ SELECT * FROM tb_personagens;
 
 INSERT INTO tb_personagens( poder_ataque, poder_defesa, nome, arma, classe_id)
 VALUES(1000.02,2000.03,"Arqueiro Verde","Arco e flecha",2);
-/*
+
 INSERT INTO tb_personagens( poder_ataque, poder_defesa, nome, arma, classe_id)
-VALUES(2340.12,3230.33,"KATANA","facas",2);
+VALUES(2340.12,3230.33,"CATANA","facas",2);
 
 INSERT INTO tb_personagens( poder_ataque, poder_defesa, nome, arma, classe_id)
 VALUES(3450.32,1430.53,"Rastro sangíneo","olfato",3);
@@ -55,3 +59,22 @@ VALUES(2300.02,3330.03,"Verme cinzento","espada",4);
 
 INSERT INTO tb_personagens( poder_ataque, poder_defesa, nome, arma, classe_id)
 VALUES(1040.42,2330.03,"Arqueiro Negro","Arco e flecha",5);
+
+/*SELECT que retorne todes os personagens cujo poder de ataque seja maior do que 2000*/
+
+SELECT * FROM tb_personagens WHERE poder_ataque>2000.00 ;
+
+/* SELECT que retorne todes os personagens cujo poder de defesa esteja no intervalo 1000 e 2000.*/
+
+SELECT * FROM tb_personagens WHERE poder_defesa BETWEEN 1000.00 AND 2000.00;
+
+SELECT * FROM tb_personagens WHERE nome LIKE "c%";
+
+/*SELECT UTILIZANDO INNER JOIN*/
+
+SELECT * FROM tb_personagens INNER JOIN tb_classes
+ON tb_classes.id = tb_personagens.id;
+
+SELECT * FROM tb_personagens INNER JOIN tb_classes
+ON tb_classes.id = tb_personagens.id WHERE posicao_ataque = "arqueiro";
+
